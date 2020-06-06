37e732719e499e5163118753f79c380a7e639f8e
DerpHoof <inleejuno@gmail.com>
2020-06-06 16:00:40 +0300

    Add ThroatCleaner logic to ConsoleAnnouncerImpl

--- src/main/java/com/n_skiy/myspring/example/ConsoleAnnouncerImpl.java
+++ src/main/java/com/n_skiy/myspring/example/ConsoleAnnouncerImpl.java
@@ -1,9 +1,15 @@
 package com.n_skiy.myspring.example;
 
+import com.n_skiy.myspring.ObjectFactory;
+
+
 public class ConsoleAnnouncerImpl implements Announcer {
+
+    private ThroatCleaner throatCleaner = ObjectFactory.getInstance().createObject(ThroatCleaner.class);
 
     @Override
     public void announce(String announce) {
+        throatCleaner.throatClean();
         System.out.println(announce);
     }
 }
