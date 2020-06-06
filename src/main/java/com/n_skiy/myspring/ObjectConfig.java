803fb25d49161f01568861034c13f55b7be78a7c
DerpHoof <inleejuno@gmail.com>
2020-06-06 11:22:34 +0300

    Add possibility to instantiate specific impl of interface

--- src/main/java/com/n_skiy/myspring/ObjectConfig.java
+++ src/main/java/com/n_skiy/myspring/ObjectConfig.java
@@ -2,23 +2,28 @@ package com.n_skiy.myspring;
 
 import org.reflections.Reflections;
 
+import java.util.Map;
 import java.util.Set;
 
 
 public class ObjectConfig {
 
     private Reflections scanner;
+    private Map<Class, Class> interface2ImplClass;
 
-    public ObjectConfig(String packageToScan) {
+    public ObjectConfig(String packageToScan, Map<Class, Class> interface2ImplClass) {
+        this.interface2ImplClass = interface2ImplClass;
         this.scanner = new Reflections(packageToScan);
     }
 
     public <T> Class<? extends T> getImplClass(Class<T> interfaceType) {
-        Set<Class<? extends T>> classes = scanner.getSubTypesOf(interfaceType);
-        if (classes.size() != 1) {
-            throw new RuntimeException(interfaceType + "has 0 or more than one impl");
-        }
+        return interface2ImplClass.computeIfAbsent(interfaceType, aClass -> {
+            Set<Class<? extends T>> classes = scanner.getSubTypesOf(interfaceType);
+            if (classes.size() != 1) {
+                throw new RuntimeException(interfaceType + "has 0 or more than one impl please update your config");
+            }
 
-        return classes.iterator().next();
+            return classes.iterator().next();
+        });
     }
 }
--- src/main/java/com/n_skiy/myspring/ObjectFactory.java
+++ src/main/java/com/n_skiy/myspring/ObjectFactory.java
@@ -1,13 +1,15 @@
 package com.n_skiy.myspring;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ObjectFactory {
 
     private static ObjectFactory instance = new ObjectFactory();
 
-    private ObjectConfig objectConfig = new ObjectConfig("com.n_skiy.myspring");
+    private ObjectConfig objectConfig;
 
 
     public static ObjectFactory getInstance() {
@@ -15,6 +17,10 @@ public class ObjectFactory {
     }
 
     private ObjectFactory() {
+        HashMap<Class, Class> interface2ImplClass = new HashMap<>();
+        interface2ImplClass.put(CatSpookier.class, SprinklerCatSpookierImpl.class);
+
+        objectConfig = new ObjectConfig("com.n_skiy.myspring", interface2ImplClass);
     }
 
     public <T> T createObject(Class<T> type) {
