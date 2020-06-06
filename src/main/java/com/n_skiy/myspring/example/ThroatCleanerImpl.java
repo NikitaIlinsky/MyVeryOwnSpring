3fd8cdb99169a771fb0e16ba63d6c2361cfaddf7
DerpHoof <inleejuno@gmail.com>
2020-06-06 15:56:50 +0300

    Add InjectStuff annotation

--- src/main/java/com/n_skiy/myspring/example/ThroatCleanerImpl.java
+++ src/main/java/com/n_skiy/myspring/example/ThroatCleanerImpl.java
@@ -2,8 +2,11 @@ package com.n_skiy.myspring.example;
 
 public class ThroatCleanerImpl implements ThroatCleaner {
 
+    @InjectStuff
+    private String coughType;
+
     @Override
     public void throatClean() {
-        System.out.println("Khe-Khe");
+        System.out.println(coughType + " " + coughType);
     }
 }
