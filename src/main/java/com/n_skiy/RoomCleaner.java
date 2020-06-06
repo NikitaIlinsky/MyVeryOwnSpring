406a47f3d997e2dde1b8a330c2fa32269ccb40c5
DerpHoof <inleejuno@gmail.com>
2020-06-06 09:58:08 +0300

    Add Announcer and CatSpooker logic to RoomCleaner

--- src/main/java/com/n_skiy/RoomCleaner.java
+++ src/main/java/com/n_skiy/RoomCleaner.java
@@ -1,10 +1,16 @@
 package com.n_skiy;
 
+
 public class RoomCleaner {
 
-    public void start(Room room) {
+    private Announcer announcer = new ConsoleAnnouncerImpl();
+    private CatSpookier catSpookier = new CatSpookierImpl();
 
+    public void start(Room room) {
+        announcer.announce("Preparing for cleaning");
+        catSpookier.spookCats();
         clean(room);
+        announcer.announce("Room " + room + " had being cleaned");
     }
 
     private void clean(Room room) {
