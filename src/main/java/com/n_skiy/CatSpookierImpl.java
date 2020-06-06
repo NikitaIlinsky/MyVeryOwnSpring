868450273ef5190a1442d6b46f383b5b3aeadfda
DerpHoof <inleejuno@gmail.com>
2020-06-06 10:08:19 +0300

    Fix extra override in implementation

--- src/main/java/com/n_skiy/CatSpookierImpl.java
+++ src/main/java/com/n_skiy/CatSpookierImpl.java
@@ -2,7 +2,6 @@ package com.n_skiy;
 
 public class CatSpookierImpl implements CatSpookier {
 
-    @Override
     public void spookCats() {
         System.out.println("Kish-Kish, furry bags!");
     }
--- src/main/java/com/n_skiy/ConsoleAnnouncerImpl.java
+++ src/main/java/com/n_skiy/ConsoleAnnouncerImpl.java
@@ -2,8 +2,7 @@ package com.n_skiy;
 
 public class ConsoleAnnouncerImpl implements Announcer {
 
-    @Override
     public void announce(String announce) {
-        System.out.println("announce");
+        System.out.println(announce);
     }
 }
