6ce6222b28f6d5d4f3528664e62bd2751a21a2e3
DerpHoof <inleejuno@gmail.com>
2020-06-07 21:45:58 +0300

    Add example of injection field by parameter

--- src/main/java/com/n_skiy/myspring/example/ThroatCleanerImpl.java
+++ src/main/java/com/n_skiy/myspring/example/ThroatCleanerImpl.java
@@ -2,11 +2,14 @@ package com.n_skiy.myspring.example;
 
 public class ThroatCleanerImpl implements ThroatCleaner {
 
-    @InjectStuff
-    private String coughType;
+    @InjectStuff                 // Inject from application.property by field name
+    private String coughType1;
+
+    @InjectStuff("coughType2")   // Inject from application.property by property
+    private String coughEnding;
 
     @Override
     public void throatClean() {
-        System.out.println(coughType + " " + coughType);
+        System.out.println(coughType1 + " " + coughEnding);
     }
 }
