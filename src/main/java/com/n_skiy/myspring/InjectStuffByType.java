f8e80ee3e474c6f8482254e54fe47c08a6c2f821
DerpHoof <inleejuno@gmail.com>
2020-06-09 15:54:38 +0300

    Fix stuff-annotations retention policy

--- src/main/java/com/n_skiy/myspring/InjectStuffByType.java
+++ src/main/java/com/n_skiy/myspring/InjectStuffByType.java
@@ -1,5 +1,11 @@
 package com.n_skiy.myspring;
 
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface InjectStuffByType {
 
 }
--- src/main/java/com/n_skiy/myspring/example/Main.java
+++ src/main/java/com/n_skiy/myspring/example/Main.java
@@ -1,9 +1,12 @@
 package com.n_skiy.myspring.example;
 
+import com.n_skiy.myspring.ObjectFactory;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        RoomCleaner roomCleaner = new RoomCleaner();
+        RoomCleaner roomCleaner = ObjectFactory.getInstance().createObject(RoomCleaner.class);
         Room room = new Room();
 
         roomCleaner.startCleaning(room);
